cfe0919bde6089e9ebd39c0430af14b3cac3a119
vedanthub1202 <85518358+vedanthub1202@users.noreply.github.com>
2023-01-27 22:53:54 +0530

    Create Search in a 2d Matrix.cpp

---
+++ Day3: Arrays III/Search in a 2d Matrix.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int i = 0, j = m-1;
+
+        while(i<n && j>-1)
+        {
+            int curr = matrix[i][j];
+            if(curr == target)
+            return true;
+            else
+            {
+                if(target > curr)
+                i++;
+                else
+                j--;
+            }
+        }
+        return false;
+    }
+};
