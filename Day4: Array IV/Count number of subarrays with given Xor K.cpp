1f68f39cd33b455dea1e42c713b14ab961d8e2a1
vedanthub1202 <85518358+vedanthub1202@users.noreply.github.com>
2023-02-03 22:41:17 +0530

    Create Count number of subarrays with given Xor K.cpp

---
+++ Day4: Array IV/Count number of subarrays with given Xor K.cpp
@@ -0,0 +1,16 @@
+int Solution::solve(vector<int> &A, int B) {
+    map<int, int> mp;
+    int cnt=0, xorr = 0;
+    for(auto it: A)
+    {
+        xorr = xorr^it;
+        if(xorr == B)
+        cnt++;
+        
+        if(mp.find(xorr ^ B) != mp.end())
+        cnt += mp[xorr ^ B];
+        
+        mp[xorr] += 1;
+    }
+    return cnt;
+}
