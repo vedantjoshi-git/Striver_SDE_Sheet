7301d43c05f285b8c9fc99cabe30cf4cf97d1767
vedanthub1202 <85518358+vedanthub1202@users.noreply.github.com>
2023-02-23 23:17:16 +0530

    Create Activity Selection (it is the same as N meeting in one room).cpp

---
+++ Day8 : Greedy Algorithm/Activity Selection (it is the same as N meeting in one room).cpp
@@ -0,0 +1,42 @@
+
+
+bool comp(pair<int,int>p1, pair<int,int> p2)
+    {
+        if(p1.second == p2.second)
+        {
+            return p1.first<p2.first;
+        }
+        return p1.second < p2.second;
+    }
+
+class Solution
+{
+    public:
+    //Function to find the maximum number of meetings that can
+    //be performed in a meeting room.
+    int maxMeetings(int start[], int end[], int n)
+    {
+        // Your code here
+        vector<pair<int,int>> ans;
+        for(int i=0;i<n;i++)
+        {
+            ans.push_back({start[i], end[i]});
+        }
+        sort(ans.begin(), ans.end(),comp);
+        
+        int count=0;
+        pair<int,int> curr = {0,0};
+        
+        
+        for(int i=0;i<n;i++)
+        {
+            if(ans[i].first > curr.second)
+            {
+                count++;
+                curr.second = ans[i].second;
+            }
+        }
+    
+        return count;
+    }
+};
